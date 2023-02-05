404676728faa7cfae29e82253c059a61fb48dc41
badr ghanbi <bghanbi50@gmail.com>
2023-02-05 17:10:22 +0100

    add some resuable classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App=()=>{
- return <h1 className="text-white">hello world</h1>
+ return(
+ <>
+ <p>
+ Right now, there is no ability to add images while giving feedback which isn't ideal because I have to use another app to show what I mean
+ </p>
+ <h1 className="text-black flex flex-col">hello world</h1>
+ <button className="btn-primary">save </button>
+ </>
+ )
 }
 
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*,jsx}"],
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
     extend: {
