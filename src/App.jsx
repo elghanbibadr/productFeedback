63a4af4e01809b3639708708cf2801cea298c808
badr ghanbi <bghanbi50@gmail.com>
2023-02-05 15:05:21 +0100

    set up the project and install the needed dependency and add the colors

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+const App=()=>{
+ return <h1 className="text-white">hello world</h1>
+}
+
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*,jsx}"],
+
+  theme: {
+    extend: {
+      colors: {
+        //       /* Primary */
+        // --radial-gradient: radial-gradient(
+        //   128.88% 128.88% at 103.9% -10.39%,
+        //   #e84d70 0%,
+        //   #a337f6 53.09%,
+        //   #28a7ed 100%
+        // );
+        purple: '#ad1fe',
+        purpleHover: '#75af6',
+        blue: '#4661e6',
+        blueHover: '#7c91f9',
+        blueLight: '#62bcfa',
+        orange: '#f49f85',
+        red: '#d73737',
+        redHover: '#e98888',
+        /* Neutral */
+        greyDarkest: '#373f68',
+        greyDarker: '#3a4374',
+        greyDarker15: 'hsl(231, 33%, 34%, 0.15)',
+        greydarkerHover: '#656ea3',
+        greyDark: '#647196',
+        greyHover: '#cfd7ff',
+        inputPlaceholder: '#8c92b3',
+        grey: '#f2f4ff',
+        grey75: 'hsl(231, 100%, 97%, 0.75)',
+        greyLight: '#f7f8fd',
+      },
+    },
+  },
+  plugins: [],
+}
