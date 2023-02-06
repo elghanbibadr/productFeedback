9dc26d82155d5649dcd32cc36c810ecdbe240116
badr ghanbi <bghanbi50@gmail.com>
2023-02-06 13:48:25 +0100

    add the btn component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Btn from "./componenet/utils/Btn";
+
 const App=()=>{
  return(
  <>
@@ -5,7 +7,11 @@ const App=()=>{
  Right now, there is no ability to add images while giving feedback which isn't ideal because I have to use another app to show what I mean
  </p>
  <h1 className="text-black flex flex-col">hello world</h1>
- <button className="btn-primary">save </button>
+ {/* <button className="btn-primary">save </button> */}
+ <h1>There is no feedback yet.</h1>
+ <h2>Frontend Mentor</h2>
+ <h3>Roadmap</h3>
+ <Btn className='bg-black ' text='Add Feedback' />
  </>
  )
 }
---
+++ src/componenet/utils/Btn.jsx
@@ -0,0 +1,6 @@
+const Btn=(props)=>{
+    return <button className={`${props.className}  py-3 px-6 rounded-xl cursor-pointer text-white text-center font-bold text-sm`}>{props.text}</button>
+}
+
+
+export default Btn;
