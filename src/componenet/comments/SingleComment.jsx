0df8a38a48790725e46959e6a31ae93acc2e9448
badr ghanbi <bghanbi50@gmail.com>
2023-02-11 13:32:21 +0100

    fix some issues on the single comment componenet

--- src/componenet/comments/SingleComment.jsx
+++ src/componenet/comments/SingleComment.jsx
@@ -4,7 +4,7 @@ const SingleComment = (props) => {
    <div id={props.id} className="mb-5 border-b-[1px] pb-6 ">
     <div className='flex-row-between'>
         <div className='flex'>
-         <img className='h-10 w-10 object-cover rounded-full mr-4' src={props.user.image} alt='person image' />
+         <img className='h-10 w-10 object-cover rounded-full mr-4 md:h-14 md:w-14' src={props.user.image} alt='person image' />
          <div>
             <h3>{props.user.name}</h3>
             <p>{props.user.username}</p>
@@ -12,7 +12,7 @@ const SingleComment = (props) => {
         </div>
         <a className='text-blue text-sm md:text-base font-semibold cursor-pointer'>Reply</a>
     </div> 
-    <p className='mt-6'>
+    <p className='mt-6 md:ml-14'>
       {props.content}
     </p>
    </div>
