d0af63acf5bb127c0323183b1d180873bc196b17
badr ghanbi <bghanbi50@gmail.com>
2023-02-11 12:28:04 +0100

    fix shit

--- src/componenet/comments/Comments.jsx
+++ src/componenet/comments/Comments.jsx
@@ -4,18 +4,19 @@ import Card from '../utils/Card'
 import SingleComment from './SingleComment'
 export const Comments = () => {
   const {currentSuggestionId,productRequests}=useContext(AppContext)
-   const [comment,setComment]=useState(undefined)
+  //  const [comment,setComment]=useState(undefined)
     const currentSuggestion=productRequests.find(item => item.id==currentSuggestionId)
     console.log(currentSuggestion.comments)
-    setComment(currentSuggestion.comments)
+    // setComment(currentSuggestion.comments)
+    const comments = currentSuggestion.comments;
 
 
-  console.log(comment)
+  console.log(comments)
 
   return (
     <Card>
     <h1>comment section</h1>
-     {comment &&  comment.map(({id,content,user})=>{
+     {comments &&  comments.map(({id,content,user})=>{
       return <SingleComment key={id} id={id} content={content} user={user} />
      })}
     </Card>
