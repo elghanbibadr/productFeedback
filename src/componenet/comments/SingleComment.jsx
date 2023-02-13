b6247d5df2e4a9375714e9cc2c48e74c8761329f
badr ghanbi <bghanbi50@gmail.com>
2023-02-13 13:33:06 +0100

    adjust the font size of the single comment tutor card

--- src/componenet/comments/SingleComment.jsx
+++ src/componenet/comments/SingleComment.jsx
@@ -4,10 +4,10 @@ const SingleComment = (props) => {
    <div id={props.id} className="mb-5 border-b-[1px] pb-6 ">
     <div className='flex-row-between'>
         <div className='flex'>
-         <img className='h-10 w-10 object-cover rounded-full mr-4 md:h-14 md:w-14' src={props.user.image} alt='person image' />
+         <img className='h-10 w-10 object-cover rounded-full mr-4 ' src={props.user.image} alt='person image' />
          <div>
-            <h3>{props.user.name}</h3>
-            <p>{props.user.username}</p>
+            <h3 className='mb-0'>{props.user.name}</h3>
+            <p className='text-sm relative bottom-2'>@{props.user.username}</p>
          </div>
         </div>
         <a className='text-blue text-sm md:text-base font-semibold cursor-pointer'>Reply</a>
