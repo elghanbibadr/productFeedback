1321ec49b5959a5e8c584748f86244c300fcfe91
badr ghanbi <bghanbi50@gmail.com>
2023-02-17 10:12:14 +0100

    create there is no feedback componenet

--- src/componenet/suggestions/Suggestions.jsx
+++ src/componenet/suggestions/Suggestions.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
 import SuggestionCard from './SuggestionCard'
 import { AppContext } from '../store/AppContext';
+import Card from '../utils/Card';
+import NoFeedbackCard from './NoFeedbackCard';
 
 const Suggestions = () => {
 
-  const { userSortByChoice } = useContext(AppContext)
-  const { productRequests,filterSuggestionCategory ,suggestions} = useContext(AppContext)
+  const { userSortByChoice , filterSuggestionCategory , suggestions} = useContext(AppContext)
   const [currentVisibleSuggestions,setCurrentVisibleSuggestions]=useState(suggestions)
 
   useEffect(()=>{
@@ -15,7 +16,6 @@ const Suggestions = () => {
     }
     const filteredSuggestions =suggestions.filter(element => element.category===filterSuggestionCategory.toLowerCase())
     setCurrentVisibleSuggestions(filteredSuggestions)
-    console.log(filteredSuggestions)
   },[filterSuggestionCategory])
   
 
@@ -41,16 +41,16 @@ const Suggestions = () => {
    setCurrentVisibleSuggestions(sorted)
   }
 
-  console.log(filterSuggestionCategory)
 
    
 
 
   return (
-    <div className='p-4'>
+    <div className='mt-6'>
        { currentVisibleSuggestions.map(({ id, description,category, title, upvotes, comments }, index) => {
         return <SuggestionCard key={index} id={id} upvotes={upvotes} category={category} commentsLength={comments ? comments.length : 0} description={description} title={title}  />
       })} 
+      {currentVisibleSuggestions.length===0 && <NoFeedbackCard/>}
     </div>
   )
 }
