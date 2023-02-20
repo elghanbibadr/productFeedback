6d3a5f8e123883cfe06793a6f08e44818b4930cf
badr ghanbi <bghanbi50@gmail.com>
2023-02-20 14:23:50 +0100

    make the go back link on the roadmapheader works

--- src/componenet/roadmap/RoadpMapHeader.jsx
+++ src/componenet/roadmap/RoadpMapHeader.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Btn from '../utils/Btn'
+import { Link } from 'react-router-dom'
 const RoadpMapHeader = () => {
-    const handleOnBackLinkClicked=()=>{
-        console.log('onBackLinkClicked')
-    }
+
   return (
     <div className='flex-row-between bg-greyDarker'>
         <div className='flex flex-col'>
              <div className='flex-row-between'>
              <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L2 5l4-4" stroke="#4661E6" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
-                {/* <img src={arrowLeft} alt="left arrow" /> */}
-                <a onClick={handleOnBackLinkClicked} href="" className='text-sm ml-3 text-greyDark font-bold'>Go Back</a>
+                <Link to="/"  className='text-sm ml-3 text-greyDark font-bold'>Go Back</Link>
             </div>
             <h2>Roadmap</h2>
         </div>
