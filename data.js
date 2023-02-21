7a4615fee6008bafa0d33abbdea3869ea9ae343c
badr ghanbi <bghanbi50@gmail.com>
2023-02-21 17:00:30 +0100

    add user images src

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
  export const data=[{
   "currentUser": {
-    "image": "user-images/image-jesse.jpg",
+    "image": "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     "name": "Jesse Ronda",
     "username": "jesse10930"
   
@@ -18,7 +18,7 @@
           "id": 1,
           "content": "Awesome idea! Trying to find framework-specific projects within the hubs can be tedious",
           "user": {
-            "image": "user-images/image-suzanne.jpg",
+            "image": "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
             "name": "Suzanne Chang",
             "username": "upbeat1811"
           },
@@ -28,7 +28,7 @@
           "id": 2,
           "content": "Please use fun, color-coded labels to easily identify them at a glance",
           "user": {
-            "image": "user-images/image-thomas.jpg",
+            "image": "https://shiny.link/rcgPG8",
             "name": "Thomas Hood",
             "username": "brawnybrave"
             
@@ -49,7 +49,7 @@
           "id": 3,
           "content": "Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.",
           "user": {
-            "image": "user-images/image-elijah.jpg",
+            "image": "http://shrtco.de/tLhU7U",
             "name": "Elijah Moss",
             "username": "hexagon.bestagon"
           },
@@ -61,7 +61,7 @@
           "id": 4,
           "content": "Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.",
           "user": {
-            "image": "user-images/image-james.jpg",
+            "image": "https://shrtco.de/7BM93q",
             "name": "James Skinner",
             "username": "hummingbird1"
           },
@@ -70,7 +70,7 @@
               "content": "While waiting for dark mode, there are browser extensions that will also do the job. Search for 'dark theme' followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.",
               "replyingTo": "hummingbird1",
               "user": {
-                "image": "user-images/image-anne.jpg",
+                "image": "https://shrtco.de/SCrq12",
                 "name": "Anne Valentine",
                 "username": "annev1990"
               }
@@ -79,7 +79,7 @@
               "content": "Good point! Using any kind of style extension is great and can be highly customizable, like the ability to change contrast and brightness. I'd prefer not to use one of such extensions, however, for security and privacy reasons.",
               "replyingTo": "annev1990",
               "user": {
-                "image": "user-images/image-ryan.jpg",
+                "image": "https://shrtco.de/UewCgh",
                 "name": "Ryan Welles",
                 "username": "voyager.344"
               }
@@ -100,7 +100,7 @@
           "id": 5,
           "content": "Much easier to get answers from devs who can relate, since they've either finished the challenge themselves or are in the middle of it.",
           "user": {
-            "image": "user-images/image-george.jpg",
+            "image": "https://shrtco.de/fKy9j2",
             "name": "George Partridge",
             "username": "soccerviewer8"
           },
@@ -121,7 +121,7 @@
           "id": 6,
           "content": "Right now, there is no ability to add images while giving feedback which isn't ideal because I have to use another app to show what I mean",
           "user": {
-            "image": "user-images/image-javier.jpg",
+            "image": "https://shrtco.de/rcgPG8",
             "name": "Javier Pollard",
             "username": "warlikeduke"
           },
@@ -132,7 +132,7 @@
           "id": 7,
           "content": "Yes I'd like to see this as well. Sometimes I want to add a short video or gif to explain the site's behavior..",
           "user": {
-            "image": "user-images/image-roxanne.jpg",
+            "image": "https://shrtco.de/tLhU7U",
             "name": "Roxanne Travis",
             "username": "peppersprime32"
           },
@@ -153,7 +153,7 @@
           "id": 8,
           "content": "I also want to be notified when devs I follow submit projects on FEM. Is in-app notification also in the pipeline?",
           "user": {
-            "image": "user-images/image-victoria.jpg",
+            "image": "https://shrtco.de/fKy9j2",
             "name": "Victoria Mejia",
             "username": "arlen_the_marlin"
           },
@@ -162,7 +162,7 @@
               "content": "Bumping this. It would be good to have a tab with a feed of people I follow so it's easy to see what challenges they’ve done lately. I learn a lot by reading good developers' code.",
               "replyingTo": "arlen_the_marlin",
               "user": {
-                "image": "user-images/image-zena.jpg",
+                "image": "https://shrtco.de/eMljUH",
                 "name": "Zena Kelley",
                 "username": "velvetround"
               }
@@ -173,7 +173,7 @@
           "id": 9,
           "content": "I've been saving the profile URLs of a few people and I check what they’ve been doing from time to time. Being able to follow them solves that",
           "user": {
-            "image": "user-images/image-jackson.jpg",
+            "image": "https://shrtco.de/TlumsR",
             "name": "Jackson Barker",
             "username": "countryspirit"
           },
@@ -202,7 +202,7 @@
           "id": 10,
           "content": "This would be awesome! It would be so helpful to see an overview of my code in a way that makes it easy to spot where things could be improved.",
           "user": {
-            "image": "user-images/image-victoria.jpg",
+            "image": "https://shrtco.de/zXN3TA",
             "name": "Victoria Mejia",
             "username": "arlen_the_marlin"
           },
@@ -213,7 +213,7 @@
           "id": 11,
           "content": "Yeah, this would be really good. I'd love to see deeper insights into my code!",
           "user": {
-            "image": "user-images/image-jackson.jpg",
+            "image": "https://shrtco.de/zY5QXp",
             "name": "Jackson Barker",
             "username": "countryspirit"
           },
@@ -233,7 +233,7 @@
           "id": 12,
           "content": "Having a path through the challenges that I could follow would be brilliant! Sometimes I'm not sure which challenge would be the best next step to take. So this would help me navigate through them!",
           "user": {
-            "image": "user-images/image-george.jpg",
+            "image": "https://shrtco.de/6KjJH4",
             "name": "George Partridge",
             "username": "soccerviewer8"
           },
@@ -253,7 +253,7 @@
           "id": 13,
           "content": "I haven't built a portfolio site yet, so this would be really helpful. Might it also be possible to choose layout and colour themes?!",
           "user": {
-            "image": "user-images/image-ryan.jpg",
+            "image": "https://shrtco.de/EXhCFt",
             "name": "Ryan Welles",
             "username": "voyager.344"
           },
@@ -274,7 +274,7 @@
           "id": 14,
           "content": "This would be great! At the moment, I'm just starting challenges in order to save them. But this means the My Challenges section is overflowing with projects and is hard to manage. Being able to bookmark challenges would be really helpful.",
           "user": {
-            "image": "user-images/image-suzanne.jpg",
+            "image": "https://shrtco.de/aRxCVM",
             "name": "Suzanne Chang",
             "username": "upbeat1811"
           },
@@ -304,7 +304,7 @@
           "id": 15,
           "content": "I'd love to see this! It always makes me so happy to see little details like these on websites.",
           "user": {
-            "image": "user-images/image-victoria.jpg",
+            "image": "https://shrtco.de/GFezYN",
             "name": "Victoria Mejia",
             "username": "arlen_the_marlin"
           },
@@ -313,7 +313,7 @@
               "content": "Me too! I'd also love to see celebrations at specific points as well. It would help people take a moment to celebrate their achievements!",
               "replyingTo": "arlen_the_marlin",
               "user": {
-                "image": "user-images/image-suzanne.jpg",
+                "image": "https://shrtco.de/2Ab2DE",
                 "name": "Suzanne Chang",
                 "username": "upbeat1811"
               }
